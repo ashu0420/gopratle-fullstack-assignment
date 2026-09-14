ab37559ed86fd21a9e2c5b56c57973429fd5707e
ashu <c.asheesh03@gmail.com>
2026-09-14 09:44:10 +0530

    Restrict CORS to production frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://gopratle-frontend-eta.vercel.app",
+    })
+  );
 app.use(express.json());
 
 // Test route
